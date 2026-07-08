63d55ae8566098c958db97257af419cd5574c3bf
val4alt <cairault.v@gmail.com>
2026-07-08 13:43:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.static('public'));
 
-// Base de données locale en mémoire (Ultra stable, pas de clé API)
 let playersDatabase = [
     { pseudo: "Coco", rank: "Phantom", note: "Mouvement, Aim et Save parfait" }
 ];
@@ -27,17 +26,26 @@ app.post('/api/players', (req, res) => {
         return res.status(400).json({ success: false, error: "Le pseudo est requis." });
     }
 
-    // On cherche si le joueur existe déjà
     const index = playersDatabase.findIndex(p => p.pseudo.toLowerCase() === pseudo.toLowerCase());
 
     if (index !== -1) {
-        // S'il existe, on le met à jour
         playersDatabase[index] = { pseudo, rank, note };
     } else {
-        // Sinon, on l'ajoute
         playersDatabase.push({ pseudo, rank, note });
     }
 
+    res.json({ success: true });
+});
+
+// 3. SUPPRESSION D'UN MEMBRE
+app.delete('/api/players', (req, res) => {
+    const { pseudo, password } = req.body;
+
+    if (password !== "PHNTM") {
+        return res.status(403).json({ success: false, error: "Clé incorrecte !" });
+    }
+
+    playersDatabase = playersDatabase.filter(p => p.pseudo.toLowerCase() !== pseudo.toLowerCase());
     res.json({ success: true });
 });
 
